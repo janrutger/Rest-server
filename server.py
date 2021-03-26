e7ef4cc9d17be70e37afbd2bda1335908a8da08a
janrutger <janrutger@jaruku.nl>
2021-03-26 17:25:59 +0100

    GET Query method I

--- server.py
+++ server.py
@@ -12,12 +12,6 @@ app.config['MONGODB_SETTINGS'] = {
 db = MongoEngine()
 db.init_app(app)
 
-# class User(db.Document):
-#     name = db.StringField()
-#     email = db.StringField()
-#     def to_json(self):
-#         return {"name": self.name,
-#                 "email": self.email}
 
 class Sensor_data(db.Document):
     sample_id  = db.StringField(primary_key=True)
@@ -42,37 +36,23 @@ class Sensor_data(db.Document):
 def query_records():
     #sampels = Sensor_data.objects.order_by("-time_for").first()
     sampels = Sensor_data.objects.order_by("-time_for")[0:10]
-    # # jr = Sensor_data.objects.order_by("-time_for")[0:2]
-    # # print(len(jr))
-    # # print(jr.to_json())
-    # # print(jr[5].to_json())
-
-    # print(len(sampels()))
-    # print(type(sampels()))
-    # print(sampels())
-    # val = {}
-    # for n in range(0, 10):
-    #     #print(sampels[n].to_json())
-    #     val[n] = sampels[n].to_json()
-    # #return jsonify(val)
     return jsonify(sampels().to_json())
 
 @app.route('/plot/<station_id>/<parameter>', methods=['GET'])
 def query_selection(station_id, parameter):
-    selection = Sensor_data.objects(station_id=station_id, parameter=parameter).only("time_for", "value").first
+    selection = Sensor_data.objects(station_id=station_id, parameter=parameter).only("sample_id", "time_for", "value")[0:5]
     if not selection():  
         result = {"ERROR" : {"STATION" : station_id, "PARAMETER" : parameter}}
         return jsonify(result)
     else:
-        return jsonify(selection().to_json())
+        xas = []
+        yas = []
+        for n in range(5):
+            xas.append(selection()[n].to_json()["time_for"])
+            yas.append(selection()[n].to_json()["value"]["T"])
+        
+        return jsonify(selection()[5].to_json())
 
-# @app.route('/', methods=['PUT'])
-# def create_record():
-#     record = json.loads(request.data)
-#     user = User(name=record['name'],
-#                 email=record['email'])
-#     user.save()
-#     return jsonify(user.to_json())
 
 @app.route('/', methods=['POST'])
 def update_record():
@@ -92,15 +72,7 @@ def update_record():
     return jsonify({"data received" : "ok"})
 
 
-# @app.route('/', methods=['DELETE'])
-# def delete_record():
-#     record = json.loads(request.data)
-#     user = User.objects(name=record['name']).first()
-#     if not user:
-#         return jsonify({'error': 'data not found'})
-#     else:
-#         user.delete()
-#     return jsonify(user.to_json())
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
