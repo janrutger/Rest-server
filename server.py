5b3b5fc5b12856534f259a6816409527b869a8a2
janrutger <janrutger@jaruku.nl>
2021-03-26 19:03:37 +0100

    First Graph

--- server.py
+++ server.py
@@ -3,6 +3,11 @@ import datetime
 from flask import Flask, request, jsonify
 from flask_mongoengine import MongoEngine
 
+import base64
+from io import BytesIO
+#import matplotlib.pyplot as Plt
+from matplotlib.figure import Figure
+
 app = Flask(__name__)
 app.config['MONGODB_SETTINGS'] = {
     'db': 'SampleData',
@@ -40,18 +45,29 @@ def query_records():
 
 @app.route('/plot/<station_id>/<parameter>', methods=['GET'])
 def query_selection(station_id, parameter):
-    selection = Sensor_data.objects(station_id=station_id, parameter=parameter).only("sample_id", "time_for", "value")[0:5]
+    selection = Sensor_data.objects(station_id=station_id, parameter=parameter).only("sample_id", "time_for", "value")[0:50]
     if not selection():  
         result = {"ERROR" : {"STATION" : station_id, "PARAMETER" : parameter}}
         return jsonify(result)
     else:
         xas = []
         yas = []
-        for n in range(5):
+        for n in range(50):
             xas.append(selection()[n].to_json()["time_for"])
             yas.append(selection()[n].to_json()["value"]["T"])
         
-        return jsonify(selection()[5].to_json())
+        fig = Figure()
+        ax = fig.subplots()
+        ax.plot(xas, yas)
+        # Save it to a temporary buffer.
+        buf = BytesIO()
+        fig.savefig(buf, format="png")
+        # Embed the result in the html output.
+        data = base64.b64encode(buf.getbuffer()).decode("ascii")
+        
+        return f"<img src='data:image/png;base64,{data}'/>"
+        
+        #return jsonify(selection()[5].to_json())
 
 
 @app.route('/', methods=['POST'])
