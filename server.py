bac5ad2beaa8f742304ec4ea08101ca56b947421
janrutger <janrutger@jaruku.nl>
2021-04-17 22:05:07 +0200

    SLICE end-point

--- server.py
+++ server.py
@@ -9,6 +9,8 @@ from io import BytesIO
 #import matplotlib.pyplot as Plt
 from matplotlib.figure import Figure
 
+import statistics as stats
+
 app = Flask(__name__)
 app.config['MONGODB_SETTINGS'] = {
     'db': 'SampleData',
@@ -17,6 +19,7 @@ app.config['MONGODB_SETTINGS'] = {
 }
 db = MongoEngine()
 db.init_app(app)
+
 
 
 class Sensor_data(db.Document):
@@ -43,6 +46,85 @@ def query_records():
     #sampels = Sensor_data.objects.order_by("-time_for").first()
     sampels = Sensor_data.objects.order_by("-time_for")[0:10]
     return jsonify(sampels().to_json())
+
+@app.route('/slice/<output>/<endtime>/<hours>/<station_id>/<parameter>', methods=['GET'])
+def query_slice(output, endtime, hours, station_id, parameter):
+    #selection = False
+    if len(endtime) == 8:
+        end = datetime.strptime(endtime, '%Y%m%d')
+    elif len(endtime) == 12:
+        end = datetime.strptime(endtime, '%Y%m%d%H%M')
+    else:
+        result = {"ERROR" : {"wrong length" : endtime}}
+        return jsonify(result)
+
+    start = end - timedelta(hours=int(hours))
+
+    selection = Sensor_data.objects(station_id=station_id, parameter=parameter, time_for__gte=start, time_for__lte=end)
+    if not selection():
+        result = {"ERROR" : {"OUTPUT"    : output,
+                             "STARTTIME" : endtime,
+                             "HOURS"     : hours,
+                             "STATION"   : station_id,
+                             "PARAMETER" : parameter}
+        }
+        return jsonify(result)
+    else:
+        xas = []
+        yas = []
+        yas1= []
+        yas2= []
+        parmKeys = list(selection()[0].to_json()["value"].keys())
+        parmUnits     = selection()[0].to_json()["units"]
+
+        for n in range(len(selection())):
+            xas.append((selection()[n].to_json()["time_for"]))
+            if len(parmKeys) == 1:
+                yas.append(selection()[n].to_json()["value"][parmKeys[0]])
+            if len(parmKeys) == 3:
+                yas.append(selection()[n].to_json()["value"][parmKeys[0]])
+                yas1.append(selection()[n].to_json()["value"][parmKeys[1]])
+                yas2.append(selection()[n].to_json()["value"][parmKeys[2]])
+        
+        avgValue = stats.mean(yas)
+        median =stats.median(yas)
+
+        lastRecord = selection.order_by("-time_for").first()
+
+        if output == "json":
+            result = {"ANSWER" : {"LAST" : lastRecord.value[parmKeys[0]],
+                                  "AVERAGE" : avgValue,
+                                  "MEDIAN" : median,
+                                  "X-AS" : xas,
+                                  "Y-AS" : yas}}
+            return(jsonify(result))
+        elif output == "plot":
+            fig = Figure()
+            fig.set_figwidth(20)
+
+            ax = fig.subplots()
+            _label = parmKeys[0] + "[" + parmUnits +"]"
+            ax.plot(xas, yas, lw=1, color="red", marker="d", label=_label)
+            if len(parmKeys) > 1:
+                _label = parmKeys[1] + "[" + parmUnits +"]"
+                ax.plot(xas, yas1, lw=1, color="green", marker="^", label=_label)
+            if len(parmKeys) > 2:
+                _label = parmKeys[2] + "[" + parmUnits +"]"
+                ax.plot(xas, yas2, lw=1, color="blue",marker="v",  label=_label)
+            ax.grid()
+            ax.legend()
+            ax.set_ylabel(parameter)
+        
+    
+            #ax.xticks(rotation=70)
+            # Save it to a temporary buffer.
+            buf = BytesIO()
+            fig.savefig(buf, format="png")
+            # Embed the result in the html output.
+            data = base64.b64encode(buf.getbuffer()).decode("ascii")
+        
+            return f"<img src='data:image/png;base64,{data}'/>"
+
 
 @app.route('/plot/<hours>/<station_id>/<parameter>', methods=['GET'])
 def query_selection(hours, station_id, parameter):
@@ -115,8 +197,7 @@ def update_record():
         sample = Sensor_data(sample_id =record[0],
                              station_id=record[1],
                              parameter =record[2],
-                             #time_at =datetime.datetime.strptime(record[3], "%Y-%m-%dT%H:%M:%S"),
-                             #time_for=datetime.datetime.strptime(record[4], "%Y-%m-%dT%H:%M:%S"),
+
                              time_at =datetime.strptime(record[3], "%Y-%m-%dT%H:%M:%S"),
                              time_for=datetime.strptime(record[4], "%Y-%m-%dT%H:%M:%S"),
                              value =record[5],
