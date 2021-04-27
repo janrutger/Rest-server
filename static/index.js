9c3861a67c94ee80b99507f74925c29532007dae
janrutger <janrutger@jaruku.nl>
2021-04-27 13:24:31 +0200

    Second parm in dashboard

--- static/index.js
+++ static/index.js
@@ -2,61 +2,78 @@ google.charts.load('current', {'packages':['line']});
 
 
 
-function refresh(){
+function refresh(parm_id){
     console.log("Func refresh")
-    let url = "http://rest:5000/slice/json/now/36/5/Amsterdam/Humidity"
-    //let url = "ServerResponse.json"
+    console.log(parm_id)
+    if (parm_id == 0){
+        url = "http://rest:5000/slice/json/today/36/5/Amsterdam/Humidity"
+    } else if (parm_id == 1){
+        url = "http://rest:5000/slice/json/today/36/5/Amsterdam/temperature"
+    } else {
+        url = "http://rest:5000/slice/json/today/36/5/Amsterdam/pressure"
+    }
 
     fetch(url)
     .then(res => res.json())
     .then(j => {console.log(j)
         return j})
-    .then(j => update(j["ANSWER"]) );
+    .then(j => update(j["ANSWER"], parm_id) );
     
 }   
 
-function update(info){
+function update(info, parm_id){
     console.log("Func update")
+    console.log(parm_id)
     console.log(info)
 
     parm = Object.keys(info["VALUE_LAST"])[0]
-    value  = info["VALUE_LAST"][parm]
     markerValue = info["VALUE_N_AVERAGE"][parm]
 
-    if (markerValue < value){
-        marker = "Higher"
-    } else if (markerValue > value){
-        marker = "Lower"
+    _stationName = info["STATION"]
+    _parmName    = info["PARAMETER"].toUpperCase()
+    _value       = info["VALUE_LAST"][parm]
+    _parmMean    = info["VALUE_AVERAGE"][parm]
+
+    if (markerValue < _value){
+        _marker = "Higher"
+    } else if (markerValue > _value){
+        _marker = "Lower"
     } else {
-        marker = "Same"
+        _marker = "Same"
     }
-   
-    document.getElementById("stationName").innerHTML=info["STATION"]
-    document.getElementById("parmName").innerHTML=info["PARAMETER"].toUpperCase()
-    document.getElementById("parmValue").innerHTML=value
-    document.getElementById("parmMean").innerHTML=info["VALUE_AVERAGE"][parm]
-    document.getElementById("parmMarker").innerHTML=marker
-
-    //plot(info["TIME_LABELS"], info["VALUE_LIST"][parm])
-
-    google.charts.setOnLoadCallback(plot(info));
+    if (parm_id == 0){
+        document.getElementById("stationName").innerHTML=_stationName
+        document.getElementById("parmName").innerHTML=_parmName
+        document.getElementById("parmValue").innerHTML=_value
+        document.getElementById("parmMean").innerHTML=_parmMean
+        document.getElementById("parmMarker").innerHTML=_marker
+    } else if (parm_id == 1){
+        document.getElementById("stationName").innerHTML=_stationName
+        document.getElementById("parmName1").innerHTML=_parmName
+        document.getElementById("parmValue1").innerHTML=_value
+        document.getElementById("parmMean1").innerHTML=_parmMean
+        document.getElementById("parmMarker1").innerHTML=_marker
+    }
+    
+    google.charts.setOnLoadCallback(plot(info, parm_id));
 }
 
-function plot(info){
+function plot(info, parm_id){
     console.log("Func plot")
     console.log(info)
 
     //JR Code
     
     parm = Object.keys(info["VALUE_LAST"])[0]
-    markerValue = info["VALUE_N_AVERAGE"][parm]
+    /*markerValue = info["VALUE_N_AVERAGE"][parm]
     if (markerValue < value){
         marker = "Higher"
     } else if (markerValue > value){
         marker = "Lower"
     } else {
         marker = "Same"
-    }
+    }*/
+
     //merge 2x1D to 1x2D array
     var xas = info["TIME_LABELS"];
     var yas = info["VALUE_LIST"][parm];
@@ -69,27 +86,30 @@ function plot(info){
     //Google Code
 
     var data = new google.visualization.DataTable();
-      data.addColumn('datetime', 'Sample time');
-      data.addColumn('number', info["PARAMETER"]);
+    data.addColumn('datetime', 'Sample time');
+    data.addColumn('number', info["PARAMETER"]);
 
       data.addRows(resultArr)
 
-      var options = {
+    var options = {
         chart: {
           title: info["PARAMETER"].toUpperCase(),
           subtitle: parm + " " + info["UNITS"]
         },
         width: 830,
         height: 200
-      };
+    };
 
-      var chart = new google.charts.Line(document.getElementById('PlotGraph'));
-
-      chart.draw(data, google.charts.Line.convertOptions(options));
+    if (parm_id == 0){
+        var chart = new google.charts.Line(document.getElementById('PlotGraph'));
+    } else if (parm_id ==1){
+        var chart = new google.charts.Line(document.getElementById('PlotGraph1'));
+    }
+    chart.draw(data, google.charts.Line.convertOptions(options));
 
 
 }
 
-function refreshButton(){
-    refresh()
+function refreshButton(num){
+    refresh(num)
 }
