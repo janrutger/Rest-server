dda9cd0edeb6987519507cb69e2166c662225e55
janrutger <janrutger@jaruku.nl>
2022-05-19 19:44:23 +0200

    More stuff

--- static/index.js
+++ static/index.js
@@ -6,11 +6,11 @@ function refresh(parm_id){
     console.log("Func refresh")
     console.log(parm_id)
     if (parm_id == 0){
-        url = "http://rest:5000/slice/json/now/170/5/Amsterdam/temperature"
+        url = "http://rest:5000/slice/json/now/170/5/BSEC-Meetstation/co2"
     } else if (parm_id == 1){
-        url = "http://rest:5000/slice/json/now/170/5/Amsterdam/Humidity"
+        url = "http://rest:5000/slice/json/now/170/5/BSEC-Meetstation/iaq"
     } else if (parm_id == 2){
-        url = "http://rest:5000/slice/json/now/170/5/Amsterdam/pressure"
+        url = "http://rest:5000/slice/json/now/170/5/BSEC-Meetstation/voc"
     }
 
     fetch(url)
